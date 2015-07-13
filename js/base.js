1500d2492fb2ce692f3da7d89242e80b8b6d19c9
310326 <mitsuru@310326.com>
2015-07-14 06:14:11 +0900

    changed new api

--- js/base.js
+++ js/base.js
@@ -1,27 +1,36 @@
 function ShowAddress() {
-	var appid = 'your-appid';
+	var appid = 'Your-AppId';
 	var query = document.getElementById('query').value;
-	var url = "http://search.olp.yahooapis.jp/OpenLocalPlatform/V1/localSearch?appid=" + appid + "&query=" + encodeURI( query ) + "&callback=ShowResult";
-//	var url = "http://placeinfo.olp.yahooapis.jp/V1/get?appid=" + appid + "&query=" + encodeURI(query) + "&callback=ShowResult";
+	var url = "http://search.olp.yahooapis.jp/OpenLocalPlatform/V1/localSearch?appid=" + appid + "&query=" + encodeURI( query ) + "&output=json&callback=ShowResult";
 	CallJSONP(url);
 }
 
 function CallJSONP(url) {
-	alert(url);
+	Message(url);
 	var target = document.createElement('script');
-	target.charset = utf-8;
+	target.charset = 'utf-8';
 	target.src = url;
 	document.body.appendChild(target);
 }
 
 function ShowResult(result) {
-	alert();
 	if(result.Count > 0)
 	{
-		alert( result.Count + "件の結果が見つかりました。¥n" + result.Item[0].Title + "の住所は" + result.Item[0].Address + "です。");
+		Message(result.Count + "件の結果が見つかりました。¥n" + result.Item[0].Title + "の住所は" + result.Item[0].Address + "です。");
 	}
 	else
 	{
-		alert("検索結果が見つかりませんでした。")
+		Message("検索結果が見つかりませんでした。")
+	}
+}
+
+function Message(message_string, type) {
+	if(type === "alert")
+	{
+		alert(message_string);
+	}
+	else
+	{
+		console.log(message_string);
 	}
 }
